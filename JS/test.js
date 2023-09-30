2194386c3ba9b567f8d9dba296cfbe807a1ce0b8
chaitu423 <chaitanyareddy59@gmai.com>
2023-09-30 13:47:28 +0530

    added js folder

---
+++ JS/test.js
@@ -0,0 +1,73 @@
+// data types
+// " chaitanyareddy"  //string
+// 1 2 3   //number
+// true false //boolean
+
+
+
+// let x=5
+// let y=6
+// let z=x+y
+// console.log(z)
+
+// console.log("chaitanya")
+
+// let name ="chaitanyareddy"
+// // console.log(name).
+
+// let data ="my fathre`s name is raju reddy"
+// console.log(data)
+// let data = `raju"" reddy`
+// console.log(data.length)
+
+
+// var x=5;
+// console.log("before",x)
+// var x=6;
+// console.log("after",x)
+
+
+// let x=5;
+// console.log("before",x)
+//  x=6;
+// console.log("after",x)
+
+
+// key words          redeclare            reassign
+//   var                yes                  yes
+//   let                no                   yes
+//   const              no                   no
+
+// const x=5;
+// console.log("before",x)
+//  x=6;
+// console.log("after",x)
+
+// let firstName="chaitanya"
+// let lastName="nomula"
+// let fullName=firstName+lastName
+// console.log("fullName",fullName)
+
+
+
+// nmu+num add cheste number vastundi
+// string+string add cheste string vastundi
+// string+num add cheste aa number+string avutundi
+// num+"num" stroing vastundi
+// note double coats lo undi adina string avutundi
+
+
+
+// let num1=5
+// let num2="6"
+// console.log(num1+num2,typeof(num1+num2))
+
+
+
+
+console.log("swaira")
+
+
+
+
+
